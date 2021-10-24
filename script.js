19bd295f48f71f7e924b0b774737aefbe5905a61
Angie <amaticorena103@hotmail.com>
2021-10-23 20:06:32 -0400

    add script.js,connecto to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement("h2");
+h2.textContent = "Hi! Get your tikcets from Hansel and Gretel";
+
+document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "We are not held liable for any cavities you obtain during your visit";
+
+document.querySelector("body").appendChild(h3);
